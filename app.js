898e77b8329fe8731e80fc81ecf2b741e99bdd25
InfoOrgs <aditya01377@gmail.com>
2022-08-22 15:50:28 +0530

    int

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const mainRoute = require('./routes/index-route')
+const panels = require('./routes/panels')
+
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +15,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.status(200).render('index.ejs');
-})
+    res.status(200).render('index');
+});
+app.use('/p', mainRoute);
+app.use( panels);
 
 
-
-
+app.get('*', (req, res) => {
+    res.render('./page/error');
+});
 app.listen(port, () => { console.log('This site is running on port ' + port) });
---
+++ routes/index-route.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router()
+
+
+router.get('/about-us', (req, res) => {
+    console.log(req)
+    res.status(200).render('./page/about-us')
+})
+router.get('/Contact-Us', (req, res) => {
+    res.status(200).render('./page/Contact-Us')
+
+})
+router.get('/about-principal', (req, res) => {
+    res.status(200).render('./page/about-principal')
+
+})
+router.get('/aboutvp', (req, res) => {
+    res.status(200).render('./page/aboutvp')
+
+})
+
+router.get('/rector', (req, res) => {
+    res.status(200).render('./page/rector')
+
+})
+router.get('/Facilities', (req, res) => {
+    res.status(200).render('./page/Facilities')
+
+})
+router.get('/Faculties', (req, res) => {
+    res.status(200).render('./page/Faculties')
+
+})
+router.get('/Fees-Structure', (req, res) => {
+    res.status(200).render('./page/Fees-Structure')
+
+})
+router.get('/Rules-Regulation', (req, res) => {
+    res.status(200).render('./page/Rules-Regulation')
+
+})
+module.exports = router;
